7caa969386b9d0b4fb6e499dafe5b0fc4e8e481f
vandriodd <ludasaadi@gmail.com>
2025-12-14 21:26:13 +0400

    feat: enhance navigation bar with icons and improve menu item layout

--- src/lib/components/common/app-navbar.tsx
+++ src/lib/components/common/app-navbar.tsx
@@ -13,44 +13,32 @@ import { Link, useLocation } from 'wouter'
 import { cn } from '@/lib/utils'
 import { ThemeSwitcher } from './theme-swticher'
 import { MultilingualLogo } from '@/assets/Logo'
+import { Icon } from '@iconify/react'
 
 export default function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const [location] = useLocation()
 
   const navLinks = [
-    { href: '/', label: 'Translator' },
-    { href: '/writer', label: 'Writer' },
-    { href: '/chat', label: 'Chat' },
-  ]
-
-  const menuItems = [
-    'Profile',
-    'Dashboard',
-    'Activity',
-    'Analytics',
-    'System',
-    'Deployments',
-    'My Settings',
-    'Team Settings',
-    'Help & Feedback',
-    'Log Out',
+    { href: '/', label: 'Translator', icon: 'mdi:translate' },
+    { href: '/writer', label: 'Writer', icon: 'mdi:pencil' },
+    { href: '/chat', label: 'Chat', icon: 'mdi:chat' },
   ]
 
   return (
-    <Navbar onMenuOpenChange={setIsMenuOpen} maxWidth="xl">
+    <Navbar
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
+      maxWidth="xl"
+    >
       <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
-          className="sm:hidden"
-        />
         <NavbarBrand className="items-center" as={Link}>
           <MultilingualLogo className="text-primary mr-2 size-6" />
           <p className="font-bold text-inherit">Multilingüal</p>
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent justify="end">
+      <NavbarContent justify="end" className="hidden md:flex">
         {navLinks.map(({ href, label }) => (
           <NavbarItem key={href} isActive={location === href}>
             <UILink
@@ -72,23 +60,32 @@ export default function App() {
         <ThemeSwitcher />
       </NavbarContent>
 
+      <NavbarContent justify="end" className="flex md:hidden">
+        <ThemeSwitcher />
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+        />
+      </NavbarContent>
+
       <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
+        {navLinks.map(({ href, label, icon }) => (
+          <NavbarMenuItem key={href} isActive={location === href}>
             <UILink
-              className="w-full"
-              color={
-                index === 2
-                  ? 'primary'
-                  : index === menuItems.length - 1
-                    ? 'danger'
-                    : 'foreground'
-              }
-              href="#"
+              className={cn(
+                location === href
+                  ? 'text-primary font-semibold'
+                  : 'text-foreground',
+                'bg-content2 hover:bg-content3 hover:text-primary flex w-full gap-4 rounded-md px-4 py-6 transition-colors duration-200',
+              )}
+              href={href}
               size="lg"
               as={Link}
+              onPress={() => setIsMenuOpen(false)}
             >
-              {item}
+              <span className="bg-content1/30 inline-flex h-12 w-12 items-center justify-center rounded-full">
+                <Icon icon={icon} />
+              </span>
+              {label}
             </UILink>
           </NavbarMenuItem>
         ))}
